5a0fd42810b7ad0cc23aa27546ac1b1c5eefcb23
mingchaoyan <mingchaoyan@gmail.com>
2016-03-16 22:15:19 +0800

    更新文章

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,6 +2,19 @@ class ArticlesController < ApplicationController
   def new
     @article = Article.new
   end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article else
+      render 'edit'
+    end
+  end
   
   def create
     @article = Article.new(article_params)
